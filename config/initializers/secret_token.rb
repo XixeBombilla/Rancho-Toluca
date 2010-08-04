585e4f1bf684ed7753829cf38403178c2f9a62ac
Your Name <tiliches_88@hotmail.com>
2010-08-04 17:55:17 -0500

    primer commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rancho::Application.config.secret_token = '7478659e3a28278482168daa9164606acfdad89f632fe8153bd826182987059c9e8f0e4606bbd7d3e20e69bb6595a12517282274fcfa1ff1eb36cb6e56f215b2'
